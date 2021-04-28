9e4c90c09570b94d03cb4200fd09cd749bbf1a18
nullprop <git@nullprop.sh>
2021-04-28 17:02:43 +0300

    Clean up, add command line params

--- bridge-c-sharp-plugin/Asset.cs
+++ bridge-c-sharp-plugin/Asset.cs
@@ -1,88 +1,150 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace bridge_c_sharp_plugin
 {
-    //This class represents the data structure of Bridge exported JSON.
-    struct Asset
-    {
-        public string resolution;
-        public int resolutionValue;
-        public string category;
-        public string type;
-        public string id;
-        public string name;
-        public string path;
-        public string textureMimeType;
-        public string averageColor;
-        public string activeLOD;
-        public string[] tags;
-        public string[] categories;
+	//This class represents the data structure of Bridge exported JSON.
+	struct Asset
+	{
+		public string resolution;
+		public int resolutionValue;
+		public string category;
+		public string type;
+		public string id;
+		public string name;
+		public string path;
+		public string textureMimeType;
+		public string averageColor;
+		public string activeLOD;
+		public string[] tags;
+		public string[] categories;
 
-        public bool isCustom;
-        public int meshVersion;
+		public bool isCustom;
+		public int meshVersion;
 
-        public List<Texture> textures;
-        public List<Geometry> geometry;
-        public List<GeometryLOD> lodList;
-        public List<PackedTextures> packedTextures;
-        public List<MetaElement> meta;
-    }
+		public List<Texture> textures;
+		public List<Geometry> geometry;
+		public List<GeometryLOD> lodList;
+		public List<PackedTextures> packedTextures;
+		public List<MetaElement> meta;
 
-    struct Texture
-    {
-        public string name;
-        public string path;
-        public string resolution;
-        public string format;
-        public string type;
-    }
+		public override string ToString()
+		{
+			var stringVal = "";
 
-    struct Geometry
-    {
-        public string path;
-        public string name;
-        public string format;
-        public string type;
-    }
+			foreach ( var field in GetType().GetFields() )
+			{
+				var value = field.GetValue(this);
 
-    struct GeometryLOD
-    {
-        public string lod;
-        public string path;
-        public string name;
-        public string format;
-        public string type;
-    }
+				if ( value is System.Collections.IList list )
+				{
+					Console.WriteLine( field.Name + ": " );
 
-    struct PackedTextures
-    {
-        public string name;
-        public string path;
-        public string resolution;
-        public string format;
-        public string type;
-        public ChannelsData channelsData;
-    }
+					if ( value is string[] stringArr )
+					{
+						foreach ( var item in list )
+						{
+							Console.WriteLine( " " + item.ToString() );
+						}
+					}
+					else
+					{
+						foreach ( var item in list )
+						{
+							Console.WriteLine( item.ToString() );
+						}
+					}
+				}
+				else
+				{
+					Console.WriteLine( field.Name + ": " + field.GetValue( this ).ToString() );
+				}
+			}
 
-    struct ChannelsData
-    {
-        public ChannelInfo Red;
-        public ChannelInfo Green;
-        public ChannelInfo Blue;
-        public ChannelInfo Alpha;
-        public ChannelInfo Grayscale;
-    }
+			return stringVal;
+		}
+	}
 
-    struct ChannelInfo
-    {
-        public string type;
-        public string channel;
-    }
+	struct Texture
+	{
+		public string name;
+		public string path;
+		public string resolution;
+		public string format;
+		public string type;
 
-    struct MetaElement
-    {
-        public string value;
-        public string name;
-        public string key;
-    }
+		public override string ToString()
+		{
+			return $" {name}:\n  path: {path}\n  resolution: {resolution}\n  format: {format}\n  type: {type}";
+		}
+	}
+
+	struct Geometry
+	{
+		public string path;
+		public string name;
+		public string format;
+		public string type;
+
+		public override string ToString()
+		{
+			return $" {name}:\n  path: {path}\n  format: {format}\n  type: {type}";
+		}
+	}
+
+	struct GeometryLOD
+	{
+		public string lod;
+		public string path;
+		public string name;
+		public string format;
+		public string type;
+
+		public override string ToString()
+		{
+			return $" {name}:\n  path: {path}\n  lod: {lod}\n  format: {format}\n  type: {type}";
+		}
+	}
+
+	struct PackedTextures
+	{
+		public string name;
+		public string path;
+		public string resolution;
+		public string format;
+		public string type;
+		public ChannelsData channelsData;
+
+		public override string ToString()
+		{
+			return $" {name}:\n  path: {path}\n  resolution: {resolution}\n  format: {format}\n  type: {type}";
+		}
+	}
+
+	struct ChannelsData
+	{
+		public ChannelInfo Red;
+		public ChannelInfo Green;
+		public ChannelInfo Blue;
+		public ChannelInfo Alpha;
+		public ChannelInfo Grayscale;
+	}
+
+	struct ChannelInfo
+	{
+		public string type;
+		public string channel;
+	}
+
+	struct MetaElement
+	{
+		public object value;
+		public string name;
+		public string key;
+
+		public override string ToString()
+		{
+			return $" {name}:\n  key: {key}\n  value: {value.ToString()}";
+		}
+	}
 }
--- bridge-c-sharp-plugin/ms_bridge_importer.cs
+++ bridge-c-sharp-plugin/ms_bridge_importer.cs
@@ -29,8 +29,6 @@ please send me an email (ajwad@quixel.se) for support.
 
 Main function is responsible for starting a thread that listens to the specified port (specified in Bridge_server.cs) for JSON data..
 
-PrintProperties is an example method that demonstrates how you the data is stored in objects.
-
 */
 
 
@@ -40,182 +38,186 @@ PrintProperties is an example method that demonstrates how you the data is store
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using CommandLine;
 
 namespace bridge_c_sharp_plugin
 {
-    class ms_bridge_importer
-    {
-        static void Main(string[] args)
-        {
-            //Starts the server in background.
-            Bridge_Server listener = new Bridge_Server();
-            listener.StartServer();
-            //Press any key will close the server and exit the console app.
-            Console.ReadLine();
-            listener.EndServer();
-        }
+	class ms_bridge_importer
+	{
+		public class Options
+		{
+			[Option( 'p', "project", Required = true, HelpText = "Project path to export assets to, e.g. \"C:/Program Files (x86)/Steam/steamapps/common/Half-Life Alyx/content/hlvr_addons/my_addon/\"" )]
+			public string ProjectPath { get; set; }
 
-        public static void AssetImporter (string jsonData)
-        {
-            List<Asset> assets = new List<Asset>();
-            //Parsing JSON array for multiple assets.
-            string jArray = jsonData;
-            JArray assetsJsonArray = JArray.Parse(jArray);
-            for (int i = 0; i < assetsJsonArray.Count; ++i)
-            {
-                //Parsing JSON data.
-                assets.Add(ImportMegascansAssets(assetsJsonArray[i].ToObject<JObject>()));
-            }
+			[Option( 'd', "directory", Required = false, Default = "/megascans", HelpText = "Directory to export assets to, relative to project root" )]
+			public string ExportDirectory { get; set; }
 
-            foreach (Asset asset in assets)
-            {
-                //Prints some values from the parsed json data.
-                PrintProperties(asset);
-            }
-        }
+			[Option( 'l', "listen", Required = false, Default = 24981, HelpText = "The port to listen on" )]
+			public int ServerPort { get; set; }
+		}
 
-        static Asset ImportMegascansAssets (JObject objectList)
-        {
-            Asset asset = new Asset();
-            //Parsing asset properties.
-            asset.name = (string)objectList["name"];
-            asset.id = (string)objectList["id"];
-            asset.type = (string)objectList["type"];
-            asset.category = (string)objectList["category"];
-            asset.path = (string)objectList["path"];
-            asset.averageColor = (string)objectList["averageColor"];
-            asset.activeLOD = (string)objectList["activeLOD"];
-            asset.textureMimeType = (string)objectList["textureFormat"];
-            asset.meshVersion = (int)objectList["meshVersion"];
-            asset.resolution = (string)objectList["resolution"];
-            asset.resolutionValue = int.Parse((string)objectList["resolutionValue"]);
-            asset.isCustom = (bool)objectList["isCustom"];
-            //Initializing asset component lists to avoid null reference error.
-            asset.textures = new List<Texture>();
-            asset.geometry = new List<Geometry>();
-            asset.lodList = new List<GeometryLOD>();
-            asset.packedTextures = new List<PackedTextures>();
-            asset.meta = new List<MetaElement>();
-            //Parse and store geometry list.
-            JArray meshComps = (JArray)objectList["meshList"];
+		static void Main( string[] args )
+		{
+			Parser.Default.ParseArguments<Options>( args )
+				.WithParsed( Run );
+		}
 
-            foreach (JObject obj in meshComps)
-            {
-                Geometry geo = new Geometry();
-                geo.name = (string)obj["name"];
-                geo.path = (string)obj["path"];
-                geo.type = (string)obj["type"];
-                geo.format = (string)obj["format"];
+		static void Run( Options options )
+		{
+			//Starts the server in background.
+			Bridge_Server listener = new Bridge_Server(options.ServerPort);
+			listener.StartServer();
 
-                asset.geometry.Add(geo);
-            }
-            //Parse and store LOD list.
-            JArray lodComps = (JArray)objectList["lodList"];
+			//New line will close the server and exit the console app.
+			Console.ReadLine();
+			listener.EndServer();
+		}
 
-            foreach (JObject obj in lodComps)
-            {
-                GeometryLOD geo = new GeometryLOD();
-                geo.name = (string)obj["name"];
-                geo.path = (string)obj["path"];
-                geo.type = (string)obj["type"];
-                geo.format = (string)obj["format"];
-                geo.lod = (string)obj["lod"];
+		public static void AssetImporter( string jsonData )
+		{
+			List<Asset> assets = new List<Asset>();
 
-                asset.lodList.Add(geo);
-            }
-            //Parse and store meta data list.
-            JArray metaData = (JArray)objectList["meta"];
+			//Parsing JSON array for multiple assets.
+			string jArray = jsonData;
+			JArray assetsJsonArray = JArray.Parse(jArray);
+			for ( int i = 0; i < assetsJsonArray.Count; ++i )
+			{
+				//Parsing JSON data.
+				assets.Add( ImportMegascansAssets( assetsJsonArray[i].ToObject<JObject>() ) );
+			}
 
-            foreach (JObject obj in metaData)
-            {
-                MetaElement mElement = new MetaElement();
-                mElement.name = (string)obj["name"];
-                mElement.value = (string)obj["value"];
-                mElement.key = (string)obj["key"];
-
-                asset.meta.Add(mElement);
-            }
-            //Parse and store textures list.
-            JArray textureComps = (JArray)objectList["components"];
-
-            foreach (JObject obj in textureComps)
-            {
-                Texture tex = new Texture();
-                tex.name = (string)obj["name"];
-                tex.path = (string)obj["path"];
-                tex.type = (string)obj["type"];
-                tex.format = (string)obj["format"];
-                tex.resolution = (string)obj["resolution"];
-
-                asset.textures.Add(tex);
-            }
-            //Parse and store channel packed textures list.
-            JArray packedTextureComps = (JArray)objectList["packedTextures"];
-
-            foreach (JObject obj in packedTextureComps)
-            {
-                PackedTextures tex = new PackedTextures();
-                tex.name = (string)obj["name"];
-                tex.path = (string)obj["path"];
-                tex.type = (string)obj["type"];
-                tex.format = (string)obj["format"];
-                tex.resolution = (string)obj["resolution"];
-
-                tex.channelsData.Red.type = (string)obj["channelsData"]["Red"][0];
-                tex.channelsData.Red.channel = (string)obj["channelsData"]["Red"][1];
-                tex.channelsData.Green.type = (string)obj["channelsData"]["Green"][0];
-                tex.channelsData.Green.channel = (string)obj["channelsData"]["Green"][1];
-                tex.channelsData.Blue.type = (string)obj["channelsData"]["Blue"][0];
-                tex.channelsData.Blue.channel = (string)obj["channelsData"]["Blue"][1];
-                tex.channelsData.Alpha.type = (string)obj["channelsData"]["Alpha"][0];
-                tex.channelsData.Alpha.channel = (string)obj["channelsData"]["Alpha"][1];
-                tex.channelsData.Grayscale.type = (string)obj["channelsData"]["Grayscale"][0];
-                tex.channelsData.Grayscale.channel = (string)obj["channelsData"]["Grayscale"][1];
-
-                asset.packedTextures.Add(tex);
-            }
-            //Parse and store categories list.
-            JArray categories = (JArray)objectList["categories"];
-            asset.categories = new string[categories.Count];
-            for (int i = 0; i < categories.Count; ++i)
-            {
-                asset.categories[i] = (string)categories[i];
-            }
-            //Parse and store tags list.
-            JArray tags = (JArray)objectList["tags"];
-            asset.tags = new string[tags.Count];
-            for (int i = 0; i < tags.Count; ++i)
-            {
-                asset.tags[i] = (string)tags[i];
-            }
-
-            return asset;
-        }
-
-        static void PrintProperties(Asset asset)
-        {
-            foreach (var prop in asset.GetType().GetProperties())
-            {
-                Console.WriteLine(prop.Name + ": " + prop.GetValue(asset, null));
-            }
-
-            foreach (var field in asset.GetType().GetFields())
-            {
-                Console.WriteLine(field.Name + ": " + field.GetValue(asset));
-            }
-
-            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            //You can access the fields like this. More information on fields can be found in Asset.cs script.
-            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            //Console.WriteLine(asset.packedTextures[0].name);
-            //Console.WriteLine(asset.packedTextures[0].path);
-            //Console.WriteLine(asset.packedTextures[0].format);
-            //Console.WriteLine(asset.packedTextures[0].channelsData.Red.type);
-            //Console.WriteLine(asset.packedTextures[0].channelsData.Red.channel);
+			foreach ( Asset asset in assets )
+			{
+				//Prints some values from the parsed json data.
+				Console.WriteLine( "\nASSET" );
+				Console.WriteLine( "- - - - - - - -\n" );
+				Console.WriteLine( asset.ToString() );
+				Console.WriteLine( "- - - - - - - -" );
 
 
-        }
-    }
+			}
+		}
+
+		static Asset ImportMegascansAssets( JObject objectList )
+		{
+			Asset asset = new Asset();
+
+			//Parsing asset properties.
+			asset.name = ( string )objectList["name"];
+			asset.id = ( string )objectList["id"];
+			asset.type = ( string )objectList["type"];
+			asset.category = ( string )objectList["category"];
+			asset.path = ( string )objectList["path"];
+			asset.averageColor = ( string )objectList["averageColor"];
+			asset.activeLOD = ( string )objectList["activeLOD"];
+			asset.textureMimeType = ( string )objectList["textureFormat"];
+			asset.meshVersion = ( int )objectList["meshVersion"];
+			asset.resolution = ( string )objectList["resolution"];
+			asset.resolutionValue = int.Parse( ( string )objectList["resolutionValue"] );
+			asset.isCustom = ( bool )objectList["isCustom"];
+
+			//Initializing asset component lists to avoid null reference error.
+			asset.textures = new List<Texture>();
+			asset.geometry = new List<Geometry>();
+			asset.lodList = new List<GeometryLOD>();
+			asset.packedTextures = new List<PackedTextures>();
+			asset.meta = new List<MetaElement>();
+
+			//Parse and store geometry list.
+			JArray meshComps = (JArray)objectList["meshList"];
+			foreach ( JObject obj in meshComps )
+			{
+				Geometry geo = new Geometry();
+				geo.name = ( string )obj["name"];
+				geo.path = ( string )obj["path"];
+				geo.type = ( string )obj["type"];
+				geo.format = ( string )obj["format"];
+
+				asset.geometry.Add( geo );
+			}
+
+			//Parse and store LOD list.
+			JArray lodComps = (JArray)objectList["lodList"];
+			foreach ( JObject obj in lodComps )
+			{
+				GeometryLOD geo = new GeometryLOD();
+				geo.name = ( string )obj["name"];
+				geo.path = ( string )obj["path"];
+				geo.type = ( string )obj["type"];
+				geo.format = ( string )obj["format"];
+				geo.lod = ( string )obj["lod"];
+
+				asset.lodList.Add( geo );
+			}
+
+			//Parse and store meta data list.
+			JArray metaData = (JArray)objectList["meta"];
+			foreach ( JObject obj in metaData )
+			{
+				MetaElement mElement = new MetaElement();
+				mElement.name = ( string )obj["name"];
+				mElement.key = ( string )obj["key"];
+				mElement.value = obj["value"];
+
+				asset.meta.Add( mElement );
+			}
+
+			//Parse and store textures list.
+			JArray textureComps = (JArray)objectList["components"];
+			foreach ( JObject obj in textureComps )
+			{
+				Texture tex = new Texture();
+				tex.name = ( string )obj["name"];
+				tex.path = ( string )obj["path"];
+				tex.type = ( string )obj["type"];
+				tex.format = ( string )obj["format"];
+				tex.resolution = ( string )obj["resolution"];
+
+				asset.textures.Add( tex );
+			}
+
+			//Parse and store channel packed textures list.
+			JArray packedTextureComps = (JArray)objectList["packedTextures"];
+			foreach ( JObject obj in packedTextureComps )
+			{
+				PackedTextures tex = new PackedTextures();
+				tex.name = ( string )obj["name"];
+				tex.path = ( string )obj["path"];
+				tex.type = ( string )obj["type"];
+				tex.format = ( string )obj["format"];
+				tex.resolution = ( string )obj["resolution"];
+
+				tex.channelsData.Red.type = ( string )obj["channelsData"]["Red"][0];
+				tex.channelsData.Red.channel = ( string )obj["channelsData"]["Red"][1];
+				tex.channelsData.Green.type = ( string )obj["channelsData"]["Green"][0];
+				tex.channelsData.Green.channel = ( string )obj["channelsData"]["Green"][1];
+				tex.channelsData.Blue.type = ( string )obj["channelsData"]["Blue"][0];
+				tex.channelsData.Blue.channel = ( string )obj["channelsData"]["Blue"][1];
+				tex.channelsData.Alpha.type = ( string )obj["channelsData"]["Alpha"][0];
+				tex.channelsData.Alpha.channel = ( string )obj["channelsData"]["Alpha"][1];
+				tex.channelsData.Grayscale.type = ( string )obj["channelsData"]["Grayscale"][0];
+				tex.channelsData.Grayscale.channel = ( string )obj["channelsData"]["Grayscale"][1];
+
+				asset.packedTextures.Add( tex );
+			}
+
+			//Parse and store categories list.
+			JArray categories = (JArray)objectList["categories"];
+			asset.categories = new string[categories.Count];
+			for ( int i = 0; i < categories.Count; ++i )
+			{
+				asset.categories[i] = ( string )categories[i];
+			}
+
+			//Parse and store tags list.
+			JArray tags = (JArray)objectList["tags"];
+			asset.tags = new string[tags.Count];
+			for ( int i = 0; i < tags.Count; ++i )
+			{
+				asset.tags[i] = ( string )tags[i];
+			}
+
+			return asset;
+		}
+	}
 }
