0affee31375540aaa6a20883601fca6d7e2f2843
nullprop <git@nullprop.sh>
2021-04-29 20:19:29 +0300

    Clean up code, renaming

--- BridgeSource2Plugin/Util.cs
+++ BridgeSource2Plugin/Util.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace bridge_c_sharp_plugin
+namespace BridgeSource2Plugin
 {
 	static class Util
 	{
